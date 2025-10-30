232fd06e0841b8b05ca39c620060cec519da48ef
Heming Hanevik <heming.hanevik@gmail.com>
2025-10-30 08:13:59 +0100

    add get joke button and displays joke from joke api

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // Main script
+import { getJoke } from "./api.js";
+
+
+const getJokeButton = document.querySelector("#getJokeButton");
+getJokeButton.addEventListener("click", async (e) => {
+    const jokeCard = document.createElement("div");
+    const jokeText = document.createElement("p");
+
+    const joke = await getJoke();
+    if (joke.joke) {
+        jokeText.textContent = `${joke.joke}`;
+    } else {
+        jokeText.textContent = `${joke.setup} \n ${joke.delivery}`;
+    }
+    jokeCard.append(jokeText);
+    document.body.appendChild(jokeCard);
+})
