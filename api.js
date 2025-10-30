5a69e08dff31011c0770f981ee7de672b8aeda31
hemingus <heming.hanevik@gmail.com>
2025-10-30 14:07:37 +0100

    add getJokeByCategory

--- api.js
+++ api.js
@@ -1,6 +1,24 @@
-export async function getJoke() {
+export async function getAnyJoke() {
     try {
         const res = await fetch("https://v2.jokeapi.dev/joke/Any", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+export async function getJokeByCategory(category) {
+    try {
+        const res = await fetch(`https://v2.jokeapi.dev/joke/${category}?amount=11`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
