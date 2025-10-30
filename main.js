1d1587f47c1956842117ffee4e363d2f98e03612
hemingus <heming.hanevik@gmail.com>
2025-10-30 14:08:31 +0100

    implement Generate multiple jokes by category

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 // Main script
-import { getJoke } from "./api.js";
+import { getJokeByCategory } from "./api.js";
 
-
+const categorySelect = document.querySelector("#jokeCategory");
 const getJokeButton = document.querySelector("#getJokeButton");
 getJokeButton.addEventListener("click", async (e) => {
-    const jokeCard = document.createElement("div");
-    const jokeText = document.createElement("p");
-
-    const joke = await getJoke();
-    if (joke.joke) {
-        jokeText.textContent = `${joke.joke}`;
-    } else {
+    const data = await getJokeByCategory(categorySelect.value);
+    const jokesArr = data.jokes;
+    for (const joke of jokesArr) {
+        const jokeCard = document.createElement("div");
+        const jokeText = document.createElement("p");
+        if (joke.joke) {
+            jokeText.textContent = `${joke.joke}`;
+        } else {
         jokeText.textContent = `${joke.setup} \n ${joke.delivery}`;
+        }
+        jokeCard.append(jokeText);
+        document.body.appendChild(jokeCard);
     }
-    jokeCard.append(jokeText);
-    document.body.appendChild(jokeCard);
 })
