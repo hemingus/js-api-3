cffe5b8c9cb4ca19116f84d28616ada96d4b70c8
Heming Hanevik <heming.hanevik@gmail.com>
2025-10-30 08:12:50 +0100

    implement fetch any from joke api

--- api.js
+++ api.js
@@ -1,9 +1,17 @@
-let jokes = null;
-
-// function getJoke() {
-//     fetch("https://v2.jokeapi.dev/joke/Any")
-//         .then(res => res.json())
-//         .then(data => {
-
-//         })
-// }
+export async function getJoke() {
+    try {
+        const res = await fetch("https://v2.jokeapi.dev/joke/Any", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch (err) {
+        console.error(err);
+        return null;
+    }
+}
