300c8da62a4e9f320839011086a9328b05d9c80a
Heming Hanevik <heming.hanevik@gmail.com>
2025-10-31 08:27:34 +0100

    add index to joke elements

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const jokeContainer = document.querySelector("#jokeContainer");
 getJokeButton.addEventListener("click", async (e) => {
     const data = await getJokeByCategory(categorySelect.value);
     const jokesArr = data.jokes;
-    for (const joke of jokesArr) {
+    jokesArr.forEach((joke, index) => {
         const jokeCard = document.createElement("div");
         jokeCard.classList.add("joke-card");
         const jokeCategory = document.createElement("h3");
-        jokeCategory.textContent = joke.category;
+        jokeCategory.textContent = `${index+1}. ${joke.category}`;
         jokeCard.append(jokeCategory);
         
         const jokeText = document.createElement("p");
@@ -23,5 +23,5 @@ getJokeButton.addEventListener("click", async (e) => {
         }
         jokeCard.append(jokeText);
         jokeContainer.append(jokeCard);
-    }
+    })
 })
