95708fab7a65418c4bb3ac841da38c31f7167c33
Heming Hanevik <heming.hanevik@gmail.com>
2025-11-02 15:02:22 +0100

    implement AI image generation on joke cards

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Main script
-import { getJokeByCategory } from "./api.js";
+import { getJokeByCategory, generateImage } from "./api.js";
 
 const categorySelect = document.querySelector("#jokeCategory");
 const getJokeButton = document.querySelector("#getJokeButton");
@@ -7,21 +7,25 @@ const jokeContainer = document.querySelector("#jokeContainer");
 
 getJokeButton.addEventListener("click", async (e) => {
     const data = await getJokeByCategory(categorySelect.value);
-    const jokesArr = data.jokes;
-    jokesArr.forEach((joke, index) => {
+    
+    
         const jokeCard = document.createElement("div");
         jokeCard.classList.add("joke-card");
         const jokeCategory = document.createElement("h3");
-        jokeCategory.textContent = `${index+1}. ${joke.category}`;
+        jokeCategory.textContent = `Category: ${data.category}`;
         jokeCard.append(jokeCategory);
+        const imageElement = document.createElement("img");
         
         const jokeText = document.createElement("p");
-        if (joke.joke) {
-            jokeText.textContent = `${joke.joke}`;
+        if (data.joke) {
+            jokeText.textContent = `${data.joke}`;
+            imageElement.src = await generateImage(`${data.joke}`);
         } else {
-        jokeText.textContent = `${joke.setup} \n ${joke.delivery}`;
+            jokeText.textContent = `${data.setup} \n ${data.delivery}`;
+            imageElement.src = await generateImage(`${data.setup} ${data.delivery}`);
         }
         jokeCard.append(jokeText);
+        jokeCard.append(imageElement);
         jokeContainer.append(jokeCard);
-    })
 })
+
