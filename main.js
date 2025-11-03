b9386a97e6e5529a7c825b01be07b25e82e576a5
hemingus <heming.hanevik@gmail.com>
2025-11-03 18:12:39 +0100

    add icon to joke cards

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Main script
 import { getJokeByCategory, generateImage } from "./api.js";
+import { getCategoryIcon } from "./utils.js";
 
 const categorySelect = document.querySelector("#jokeCategory");
 const getJokeButton = document.querySelector("#getJokeButton");
@@ -7,25 +8,29 @@ const jokeContainer = document.querySelector("#jokeContainer");
 
 getJokeButton.addEventListener("click", async (e) => {
     const data = await getJokeByCategory(categorySelect.value);
+
+    const jokeCard = document.createElement("div");
+    jokeCard.classList.add("joke-card");
+    const jokeHeader = document.createElement("div");
+    const jokeCategory = document.createElement("h3");
+    const jokeIcon = document.createElement("img");
+    jokeIcon.src = getCategoryIcon(data.category);
+    jokeCategory.append(jokeIcon);
+    jokeCategory.textContent = `${data.category}`;
+    jokeHeader.append(jokeIcon, jokeCategory);
+    jokeCard.append(jokeHeader);
+    const imageElement = document.createElement("img");
     
-    
-        const jokeCard = document.createElement("div");
-        jokeCard.classList.add("joke-card");
-        const jokeCategory = document.createElement("h3");
-        jokeCategory.textContent = `Category: ${data.category}`;
-        jokeCard.append(jokeCategory);
-        const imageElement = document.createElement("img");
-        
-        const jokeText = document.createElement("p");
-        if (data.joke) {
-            jokeText.textContent = `${data.joke}`;
-            imageElement.src = await generateImage(`${data.joke}`);
-        } else {
-            jokeText.textContent = `${data.setup} \n ${data.delivery}`;
-            imageElement.src = await generateImage(`${data.setup} ${data.delivery}`);
-        }
-        jokeCard.append(jokeText);
-        jokeCard.append(imageElement);
-        jokeContainer.append(jokeCard);
+    const jokeText = document.createElement("p");
+    if (data.joke) {
+        jokeText.textContent = `${data.joke}`;
+        imageElement.src = await generateImage(`${data.joke}`);
+    } else {
+        jokeText.textContent = `${data.setup} \n ${data.delivery}`;
+        imageElement.src = await generateImage(`${data.setup} ${data.delivery}`);
+    }
+    jokeCard.append(jokeText);
+    jokeCard.append(imageElement);
+    jokeContainer.append(jokeCard);
 })
 
